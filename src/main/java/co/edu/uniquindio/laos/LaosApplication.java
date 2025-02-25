1042a2ca43a3f59d91cc8741b5b1cb560bd07800
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2025-02-25 14:29:03 -0500

    first commit

---
+++ src/main/java/co/edu/uniquindio/laos/LaosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.laos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaosApplication.class, args);
+    }
+
+}
