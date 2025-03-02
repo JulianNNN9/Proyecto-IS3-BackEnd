fc254bda6dd73cfed8239f1b02b3707ed7aa891b
Nicolás Jurado <99767358+NicholashiG@users.noreply.github.com>
2025-03-01 23:18:05 -0500

    Opciones básicas controlador público
    - Se hacen las opciones de iniciar sesion, crear usuario y recuperar contraseña
    
    - Se agrega la conexión a Mongo local, con el nombre de laosprueba

--- src/main/java/co/edu/uniquindio/laos/controllers/PublicoControlador.java
+++ src/main/java/co/edu/uniquindio/laos/controllers/PublicoControlador.java
@@ -1,5 +1,11 @@
 package co.edu.uniquindio.laos.controllers;
 
+import co.edu.uniquindio.laos.dto.MensajeDTO;
+import co.edu.uniquindio.laos.dto.TokenDTO;
+import co.edu.uniquindio.laos.dto.cuenta.CrearUsuarioDTO;
+import co.edu.uniquindio.laos.dto.cuenta.IniciarSesionDTO;
+import co.edu.uniquindio.laos.dto.cuenta.RecuperarContraseniaDTO;
+import co.edu.uniquindio.laos.services.interfaces.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +17,27 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PublicoControlador {
 
-    /*
-    private final EventoService eventoService;
-    private final UsuarioService usuarioService;
-     */
 
+    //private final EventoService eventoService;
+    private final UsuarioService usuarioService;
+
+
+    @PostMapping("/iniciar-sesion")
+    public ResponseEntity<MensajeDTO<TokenDTO>> iniciarSesion(@RequestBody IniciarSesionDTO iniciarSesionDTO) throws Exception{
+        return ResponseEntity.ok().body( new MensajeDTO<>(false, usuarioService.iniciarSesion(iniciarSesionDTO)) );
+    }
+
+    @PostMapping("/crear-usuario")
+    public ResponseEntity<MensajeDTO<String>> crearUsuario(@Valid @RequestBody CrearUsuarioDTO crearUsuarioDTO)throws Exception{
+        usuarioService.crearUsuario(crearUsuarioDTO);
+        return ResponseEntity.ok().body( new MensajeDTO<>(false, "Usuario registrado correctamente")
+        );
+    }
+
+    @PostMapping("/recuperar-contrasenia")
+    public ResponseEntity<MensajeDTO<String>> recuperarContrasenia(@RequestBody RecuperarContraseniaDTO recuperarContraseniaDTO) throws Exception{
+        usuarioService.recuperarContrasenia(recuperarContraseniaDTO);
+        return ResponseEntity.ok().body( new MensajeDTO<>(false, "Contraseña recuperada correctamente") );
+    }
 
 }
