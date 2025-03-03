120b2ad03fd90775c46fc1e58ac52c57319099a8
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2025-03-03 09:46:12 -0500

    Swagger funcionando.

--- src/main/java/co/edu/uniquindio/laos/controllers/PublicoControlador.java
+++ src/main/java/co/edu/uniquindio/laos/controllers/PublicoControlador.java
@@ -1,5 +1,10 @@
 package co.edu.uniquindio.laos.controllers;
 
+import co.edu.uniquindio.laos.dto.MensajeDTO;
+import co.edu.uniquindio.laos.dto.TokenDTO;
+import co.edu.uniquindio.laos.dto.cuenta.CrearUsuarioDTO;
+import co.edu.uniquindio.laos.dto.cuenta.IniciarSesionDTO;
+import co.edu.uniquindio.laos.services.interfaces.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +16,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PublicoControlador {
 
-    /*
-    private final EventoService eventoService;
     private final UsuarioService usuarioService;
-     */
 
+    @PostMapping("/iniciar-sesion")
+    public ResponseEntity<MensajeDTO<TokenDTO>> iniciarSesion(@RequestBody IniciarSesionDTO iniciarSesionDTO) throws Exception{
+        return ResponseEntity.ok().body( new MensajeDTO<>(false, usuarioService.iniciarSesion(iniciarSesionDTO)) );
+    }
+
+    @PostMapping("/crear-usuario")
+    public ResponseEntity<MensajeDTO<String>> crearUsuario(@Valid @RequestBody CrearUsuarioDTO crearUsuarioDTO)throws Exception{
+        usuarioService.crearUsuario(crearUsuarioDTO);
+        return ResponseEntity.ok().body( new MensajeDTO<>(false, "Usuario registrado correctamente")
+        );
+    }
 
 }
