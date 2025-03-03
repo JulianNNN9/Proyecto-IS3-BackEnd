422cb97ae3ebee33cb5d6048b8b9b3495682a615
Nicolás Jurado <99767358+NicholashiG@users.noreply.github.com>
2025-03-03 09:25:37 -0500

    Funciona swagger + update en Controlador Publico

--- src/main/java/co/edu/uniquindio/laos/controllers/PublicoControlador.java
+++ src/main/java/co/edu/uniquindio/laos/controllers/PublicoControlador.java
@@ -2,6 +2,7 @@ package co.edu.uniquindio.laos.controllers;
 
 import co.edu.uniquindio.laos.dto.MensajeDTO;
 import co.edu.uniquindio.laos.dto.TokenDTO;
+import co.edu.uniquindio.laos.dto.cuenta.ActivarCuentaDTO;
 import co.edu.uniquindio.laos.dto.cuenta.CrearUsuarioDTO;
 import co.edu.uniquindio.laos.dto.cuenta.IniciarSesionDTO;
 import co.edu.uniquindio.laos.dto.cuenta.RecuperarContraseniaDTO;
@@ -40,4 +41,20 @@ public class PublicoControlador {
         return ResponseEntity.ok().body( new MensajeDTO<>(false, "Contraseña recuperada correctamente") );
     }
 
+    @GetMapping("/enviar-codigo-recuperacion")
+    public ResponseEntity<MensajeDTO<String>> enviarCodigoRecuperacionCuenta(@RequestParam String correo) throws Exception{
+        usuarioService.enviarCodigoRecuperacionCuenta(correo);
+        return ResponseEntity.ok().body( new MensajeDTO<>(false, "Si su Correo está registrado con nosotros, su código de recuperacion fue enviado correctamente") );
+    }
+    @GetMapping("/enviar-codigo-activacion")
+    public ResponseEntity<MensajeDTO<String>> enviarCodigoActicacionCuenta(@RequestParam String correo) throws Exception{
+        usuarioService.enviarCodigoActivacionCuenta(correo);
+        return ResponseEntity.ok().body( new MensajeDTO<>(false, "Si su Correo está registrado con nosotros, su código de activacion fue enviado correctamente") );
+    }
+
+    @PostMapping("/activar-cuenta")
+    public ResponseEntity<MensajeDTO<String>> activarCuenta(@RequestBody ActivarCuentaDTO activarCuentaDTO) throws Exception{
+        usuarioService.activarCuenta(activarCuentaDTO);
+        return ResponseEntity.ok().body( new MensajeDTO<>(false, "Cuenta activada correctamente") );
+    }
 }
